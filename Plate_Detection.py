82e10742d886b0a8591242cbb8604a2f01f7981d
zzcongkunzz <dangcong200502@gmail.com>
2024-09-08 17:35:02 +0700

    train Plate

---
+++ Plate_Detection.py
@@ -0,0 +1,8 @@
+from ultralytics import YOLO
+
+# Load a pretrained YOLO model (recommended for training)
+model = YOLO("yolov10x.pt")
+
+# Train the model using the 'coco8.yaml' dataset for 3 epochs
+results = model.train(data="dataset\LP_detection.yaml", epochs=5)
+
